ff5a4e0f79ebc6d98cd02850e516817d2766aa54
dodytrifa <dody.trifa@gmail.com>
2021-04-23 22:23:09 +0700

    setup dashboard

--- src/context/context.js
+++ src/context/context.js
@@ -10,8 +10,12 @@ const GithubContext = React.createContext()
 
 //component
 const GithubProvider = ({children}) => {
+  const [githubUser, setGithubUser] = useState(mockUser) 
+  const [repos, setRepos] = useState(mockRepos) 
+  const [followers, setFollowers] = useState(mockFollowers) 
+
   return (
-    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>
   )
 }
 
