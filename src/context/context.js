d0ca642bb69cc0d1366cc6c62e3e8fbfd3a8f5a0
dodytrifa <dody.trifa@gmail.com>
2021-05-14 16:23:36 +0700

    refactor promise all

--- src/context/context.js
+++ src/context/context.js
@@ -11,18 +11,46 @@ const GithubProvider = ({children}) => {
   const [githubUser, setGithubUser] = useState(mockUser) 
   const [repos, setRepos] = useState(mockRepos) 
   const [followers, setFollowers] = useState(mockFollowers)
-  
   const [requests, setRequests] =  useState(0)
   const [isLoading, setIsLoading] = useState(false)
-
   const [error, setError] =  useState({show: false, mg:""})
+
   const searchGithubUser =  async(user) => {
     toggleError()
     setIsLoading(true)
-    const response = await axios(`${rootUrl}/users/${user}`).catch(err => console.log(err))
-    console.log(response);
+    const response = await axios(`${rootUrl}/users/${user}`)
+    .catch(err => console.log(err))
+    
     if(response) {
       setGithubUser(response.data)
+      const {login, followers_url} = response.data
+
+      //* bentuk awal
+      /*
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`)
+      .then(response => setRepos(response.data))
+
+      axios(`${followers_url}?per_page=100`)
+      .then(response => setFollowers(response.data))
+      */
+
+      //* refactor ke promise All
+      await Promise.all([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`)
+      ])
+      .then((results) => {
+        const [repos, followers] = results
+        const status = 'fulfilled'
+        if(repos.status === status) {
+          setRepos(repos.value.data)
+        }
+        if(followers.status === status) {
+          setFollowers(followers.value.data)
+        }
+      })
+      .catch(err => console.log(err))
+
     } else {
       toggleError(true,'there is no user with that username')
     }
